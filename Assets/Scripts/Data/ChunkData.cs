525055fee78695fb8f9488e7ee314972db08e4c3
NicholasAt <1965175291asd@gmail.com>
2026-09-14 13:39:10 +0300

    refactoring of chunks

--- Assets/Scripts/Data/ChunkData.cs
+++ Assets/Scripts/Data/ChunkData.cs
@@ -23,70 +23,15 @@ namespace Assets.Scripts.Data
             }
         }
 
-#if UNITY_EDITOR
-        [Header("Editor settings")]
-        [SerializeField] private float _editorChunkSize = 500;
-        [SerializeField] private List<GameObject> _chunksEditor;
-#endif
-        [Header("Main settings")]
         [SerializeField] private List<ChunkConfig> _chunkConfigs;
         public IList<ChunkConfig> ChunkConfigs => _chunkConfigs;
         private Dictionary<Vector2Int, ChunkConfig> _cachedConfigs;
         public IDictionary<Vector2Int, ChunkConfig> CachedConfigs => _cachedConfigs;
 
         [field: SerializeField] public int ChunkCount { get; private set; } = 3;
-        [field: SerializeField] public int Size { get; private set; } = 500;
+        [field: SerializeField] public int Size { get; private set; } = 626;
         [field: SerializeField] public int LoadDistance { get; private set; } = 900;
         [field: SerializeField] public int UnloadDistance { get; private set; } = 950;
-
-#if UNITY_EDITOR
-        [ContextMenu("Cache/Run")]
-        private async UniTask Cache()
-        {
-            List<AssetReferenceGameObject> chunks = new();
-            foreach (GameObject item in _chunksEditor)
-            {
-                string path = UnityEditor.AssetDatabase.GetAssetPath(item);
-                string guid = UnityEditor.AssetDatabase.AssetPathToGUID(path);
-
-                AssetReferenceGameObject assetRef = new AssetReferenceGameObject(guid);
-                chunks.Add(assetRef);
-            }
-
-            HashSet<AsyncOperationHandle> handles = new();
-            _chunkConfigs = new();
-
-            foreach (AssetReferenceGameObject reference in chunks)
-            {
-                AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(reference);
-
-                handles.Add(handle);
-                GameObject prefab = await handle;
-
-                Vector2Int pos;
-                if (prefab.TryGetComponent(out Terrain terrain))
-                {
-                    Vector3 terrainPos = prefab.transform.position + terrain.terrainData.size / 2;
-                    pos = new((int)terrainPos.x, (int)terrainPos.z);
-                }
-                else
-                {
-                    pos = new((int)prefab.transform.position.x, (int)prefab.transform.position.z);
-                    pos += new Vector2Int((int)_editorChunkSize / 2, (int)_editorChunkSize / 2);
-                }
-
-                ChunkConfig cfg = new ChunkConfig(pos, reference);
-                _chunkConfigs.Add(cfg);
-            }
-
-            foreach (AsyncOperationHandle handle in handles)
-            {
-                if (handle.IsValid())
-                    Addressables.Release(handle);
-            }
-            UnityEditor.EditorUtility.SetDirty(this);
-        }
-#endif
 
         public void Init()
         {
@@ -96,6 +41,10 @@ namespace Assets.Scripts.Data
         public bool TryConfig(Vector2Int key, out ChunkConfig cfg)
         {
             return _cachedConfigs.TryGetValue(key, out cfg);
+        }
+        public void SetConfigs(List<ChunkConfig> chunkConfigs)
+        {
+            _chunkConfigs = chunkConfigs;
         }
     }
 }
---
+++ Assets/Scripts/Editor/ChunkDataEditor.cs
@@ -0,0 +1,119 @@
+﻿using Assets.Scripts.Data;
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using static Assets.Scripts.Data.ChunkData;
+
+namespace Assets.Scripts.Editor
+{
+    [CustomEditor(typeof(ChunkData))]
+    public class ChunkDataEditor : UnityEditor.Editor
+    {
+        private string _path = "Assets/Prefabs/Chunks";
+        private Material _terrainMaterial;
+        private bool _running;
+        private float _progress;
+        public override void OnInspectorGUI()
+        {
+            ChunkData data = (ChunkData)target;
+            _path = EditorGUILayout.TextField("path", _path);
+            _terrainMaterial = (Material)EditorGUILayout.ObjectField("Terrain Material", _terrainMaterial, typeof(Material), false);
+
+            EditorGUILayout.Separator(); // Add space
+
+            if (_running == false)
+            {
+                if (GUILayout.Button("Cache"))
+                {
+                    Run(data);
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox($"click to progress [{_progress:P1}...]", MessageType.Error);
+            }
+            DrawDefaultInspector();
+        }
+        private void Run(ChunkData data)
+        {
+            if (_running)
+                return;
+
+            _running = true;
+            Cache(data).ContinueWith(() => _running = false);
+        }
+        private async UniTask Cache(ChunkData target)
+        {
+            string[] guids = AssetDatabase.FindAssets("t:GameObject", new[] { _path });
+
+            List<GameObject> assets = new List<GameObject>();
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                GameObject mat = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                if (mat != null)
+                    assets.Add(mat);
+            }
+
+            List<AssetReferenceGameObject> chunks = new();
+            foreach (GameObject item in assets)
+            {
+                string path = AssetDatabase.GetAssetPath(item);
+                string guid = AssetDatabase.AssetPathToGUID(path);
+
+                AssetReferenceGameObject assetRef = new AssetReferenceGameObject(guid);
+                chunks.Add(assetRef);
+            }
+
+            HashSet<AsyncOperationHandle> handles = new();
+            List<ChunkConfig> chunkConfigs = new();
+            try
+            {
+                for (int i = 0; i < chunks.Count; i++)
+                {
+                    AssetReferenceGameObject reference = chunks[i];
+                    AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(reference);
+                    _progress = (float)i / chunks.Count;
+                    handles.Add(handle);
+                    GameObject prefab = await handle;
+
+                    Vector2Int pos;
+                    if (prefab.TryGetComponent(out Terrain terrain))
+                    {
+                        Vector3 terrainPos = prefab.transform.position + terrain.terrainData.size / 2;
+                        pos = new((int)terrainPos.x, (int)terrainPos.z);
+                        if (_terrainMaterial != null)
+                        {
+                            terrain.materialTemplate = _terrainMaterial;
+                            EditorUtility.SetDirty(terrain);
+                        }
+                    }
+                    else
+                    {
+                        pos = new((int)prefab.transform.position.x, (int)prefab.transform.position.z);
+                        pos += new Vector2Int(target.Size / 2, target.Size / 2);
+                    }
+
+                    ChunkConfig cfg = new ChunkConfig(pos, reference);
+                    chunkConfigs.Add(cfg);
+                }
+
+                foreach (AsyncOperationHandle handle in handles)
+                {
+                    if (handle.IsValid())
+                        Addressables.Release(handle);
+                }
+                target.SetConfigs(chunkConfigs);
+                EditorUtility.SetDirty(target);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
