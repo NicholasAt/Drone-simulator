8682223233d0b9e4beb70c5c11c74bcbb6ca8a90
NicholasAt <1965175291asd@gmail.com>
2026-09-15 19:18:41 +0300

    add a controls guide

--- Assets/Scripts/UI/Windows/UIHUD/HUD.cs
+++ Assets/Scripts/UI/Windows/UIHUD/HUD.cs
@@ -1,4 +1,6 @@
+using Assets.Scripts.Data.Quests;
 using Assets.Scripts.Services;
+using Assets.Scripts.Services.GameProgress;
 using Assets.Scripts.Services.GameStates;
 using Cysharp.Threading.Tasks;
 using TMPro;
@@ -11,6 +13,7 @@ namespace Assets.Scripts.UI.Windows.UIHUD
     public class HUD : MonoBehaviour
     {
         [SerializeField] private Button _menuButton;
+        [SerializeField] private TMP_Text _guideText;
         [SerializeField] private TMP_Text _timerText;
         [SerializeField] private TMP_Text _speedText;
 
@@ -18,21 +21,24 @@ namespace Assets.Scripts.UI.Windows.UIHUD
         private GameStateMachine _gameStateMachine;
         private TimerService _timerService;
         private GameObserver _gameObserver;
+        private TempLevelProgress _levelProgress;
         private bool _inProcess;
 
         [Inject]
-        private void Construct(UIFactory uIFactory, GameStateMachine gameStateMachine, TimerService timerService, GameObserver gameObserver)
+        private void Construct(UIFactory uIFactory, GameStateMachine gameStateMachine, TimerService timerService, GameObserver gameObserver, ProgressService progressService)
         {
             _uIFactory = uIFactory;
             _gameStateMachine = gameStateMachine;
             _timerService = timerService;
             _gameObserver = gameObserver;
+            _levelProgress = progressService.TempLevelProgress;
         }
 
         private void Start()
         {
             _menuButton.onClick.AddListener(() => HomePopup().Forget(Debug.LogError));
             _timerService.OnTick += RefreshTimer;
+            RefreshTrasnportGuide();
         }
         private void Update()
         {
@@ -93,6 +99,28 @@ namespace Assets.Scripts.UI.Windows.UIHUD
             finally
             {
                 _inProcess = false;
+            }
+        }
+        private void RefreshTrasnportGuide()
+        {
+            switch (_levelProgress.QuestID)
+            {
+                case QuestID.Helicopter_Move:
+                case QuestID.Helicopter_Delivery:
+                    _guideText.text = "WASD  Q/E  Shift/Space";
+                    break;
+
+                case QuestID.Drone_Move:
+                case QuestID.Drone_DestroyMovingCar:
+                case QuestID.Drone_DestroyFlyingObjects:
+                case QuestID.Drone_DestroyStatic:
+                    _guideText.text = "WASD  Q/E  Space";
+                    break;
+
+                case QuestID.None:
+                default:
+                    Debug.LogError("no id");
+                    break;
             }
         }
     }
